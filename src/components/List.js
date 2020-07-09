0666592fcac5d30d4dc8b1f2229453a8291cc8d1
Chaw Chit <chawchitsuthwe3699@gmail.com>
2020-07-09 22:41:46 +0630

    Update

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import './List.css'
-import Axios from "axios";
+import React from 'react';
+import './List.css';
+import Card from './Card'
 
-const List = ({listId,url,title}) => {
-
-	const listActionPopup = document.getElementById("list-action-popup");
-	console.log(listActionPopup)
-
-	const [list, setList] = useState({});
+const List = ({url, listId, title, cards, displayListActionPopup}) => {
 
 	return (
 		<div>
 			<div className="list">
 				<div className="d-flex justify-content-between align-items-center mb-1">
 	  				<h6 className="pl-2">{title}</h6>
-	  				<button id="list-action-btn" className="btn btn-sm"><i className="fa fa-ellipsis-h"></i></button>
+	  				<button id="list-action-btn" className="btn btn-sm" onClick={ (e)=> displayListActionPopup(e, listId) }><i className="fa fa-ellipsis-h"></i></button>
+				</div>
+
+				{cards && cards.map(card => (
+	      
+	        	card.status === 1 && <Card key={card.id} title={card.title} labels={card.labels} accounts={card.accounts} />
+	    	
+	    		))}
+	    		<div className="d-flex justify-content-between align-items-center">
+	  				<button className="btn btn-sm btn-new-card text-left w-100" id="add-new-card">
+	    				<i className="fa fa-plus"></i>&nbsp;&nbsp;Add another card
+	  				</button>
+	  				<button className="btn btn-sm"><i className="fa fa-window-restore"></i></button>
 				</div>
 
 					{/* ${getCard(list[i].cards)} 
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import "./Wrapper.css";
 import Axios from "axios";
 
-import List from './List'
-import AddNewListBtn from './AddNewListBtn'
+import List from './List';
+import AddNewListBtn from './AddNewListBtn';
+import ListAction from './ListAction';
 
 const Wrapper = () => {
 	const url = "http://localhost:8181/";
 
 	const [lists, setLists] = useState([]);
-	// const [list, setList] = useState({});
+	const [listId, setListId] = useState("");
 
-	// const listActionPopup = document.getElementById("list-action-popup");
-	// console.log(listActionPopup)
+	const listActionPopup = document.getElementById("list-action-popup");
 
 	const fetchLists = async () => {
 		try {
@@ -24,54 +24,32 @@ const Wrapper = () => {
 		}
 	};
 
-	// const displayListActionPopup = (e) => {
-	// 	e.stopPropagation();
-
-	// 	let btn = e.target;
-	// 	if(btn.nodeName === "i" || btn.nodeName === "I") {
-	// 	btn = btn.parentNode;
-	// 	}
-	// 	const loc = btn.getBoundingClientRect();
-	// 	console.log(loc);
-	// 	listActionPopup.style.top = loc.top + loc.height + 5 + "px";
-	// 	listActionPopup.style.left = loc.left + "px";
-	// 	toggelListActionPopup(true);
-
-	// }
-
-	// const toggelListActionPopup = (isOpen) => {
-	// 	if(listActionPopup) {
-	// 		listActionPopup.style.display = isOpen ? "block":"none";
-	// 	}
-	// }
-
-	// const deleteList = () => {
-
-
-	// }
-
-	// const archiveList = () => {
-
-
-	// }
-
-	// const showListDataInEdit = () => {
-	// 	try {
-	//   		Axios.get(url + "list/" + listId)
-	//   		.then( res => {
-	//   			setList(res.data);
-	//   			toggelListActionPopup(false);
-
-	//   		})
-	// 	} 
-	// 	catch (error) {
-	//   		setList({});
-	// 	}
-	// }
-
 	useEffect(() => {
 		fetchLists();
 	}, []);
+
+	const displayListActionPopup = (e, listId) => {
+
+		setListId(listId);
+		e.stopPropagation();
+
+		let btn = e.target;
+		if(btn.nodeName === "i" || btn.nodeName === "I") {
+		btn = btn.parentNode;
+		}
+		const loc = btn.getBoundingClientRect();
+		//console.log(loc);
+		listActionPopup.style.top = loc.top + loc.height + 5 + "px";
+		listActionPopup.style.left = loc.left + "px";
+		toggelListActionPopup(true);
+
+	}
+
+	const toggelListActionPopup = (isOpen) => {
+		if(listActionPopup) {
+			listActionPopup.style.display = isOpen ? "block":"none";
+		}
+	}
 
 	return (
 		<div>
@@ -79,43 +57,14 @@ const Wrapper = () => {
 
 			{lists && lists.map(list => (
 	      
-	        	list.status === 1 && <List key={list.id} url={url} title={list.title} listId={list.id} />
+	        	list.status === 1 && <List key={list.id} url={url} title={list.title} listId={list.id} cards={list.cards} displayListActionPopup={displayListActionPopup} />
 	    	
 	    	))}
 
 	    	<AddNewListBtn url={url} maxListPos={lists.length}/>
 
 			</div>
-
-
-			{/* <div className="card list-action-card" id="list-action-popup">
-				<ul className="list-group list-group-flush">
-			  		<li className="list-group-item text-secondary">
-			            <div className="d-flex justify-content-between align-items-center">
-			              <p style= {{ fontSize: "18px" }} >List Actions</p>
-			              <button className="btn btn-sm p-0 text-danger" onClick= { () => toggelListActionPopup(false) } style= {{ fontSize: "medium" }} ><i className="fa fa-close"></i></button>
-			            </div>
-			  		</li>
-					<a href="#edit-list-modal" data-toggle="modal" className="list-group-item list-group-item-action" id="edit-list-btn">Edit</a>
-					<button onClick={ archiveList } className="list-group-item list-group-item-action">Archive This List</button>
-					<button onClick={ deleteList } className="list-group-item list-group-item-action">Delete This List</button>
-				</ul>
-			</div>
-
-			<div className="modal fade" id="edit-list-modal" tabIndex="-1" role="dialog" aria-hidden="true">
-				<div className="modal-dialog">
-				<div className="modal-content">
-						<div className="modal-body">
-							<form id="list-edit-form">
-								<div className="form-group">
-								<input type="text" className="form-control" id="editListTitle" placeholder="Enter list title" />
-								</div>
-								<button type="submit" className="btn btn-green">Edit</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div> */}
+			<ListAction url={url} listId={listId} toggelListActionPopup={toggelListActionPopup}/>
 		</div>
 
 	);
